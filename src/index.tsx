03d90c9278af7a35b86e77955135a1d99bc8696c
Carlos A. <carlosvsk@gmail.com>
2025-03-26 14:23:42 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -29,19 +29,7 @@
 
           const initCt = () => (
  {
- 'all': { 
-'colors': { 
-'primary': "#fff", 'secondary': "red" } 
-, 
-'cond1': false, 
-'func1': (nativeEvent)=>console.log(nativeEvent), 
-'Comp1': <RN.Text style={{color: "darkred"}}>---</RN.Text>, 
-'lists': { 
-'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}], 
-'lst2': [{name: "Tarefa 1", date: "21/03/25"},{name: "Tarefa 2", date: "21/03/25"},{name: "Tarefa 3", date: "24/03/25"}], 'lst3': [{name: "Task 1"},{name: "Task 2"}] } 
-, 
-'name': "Name:", 'dataToSet': {name: "Carlos", phone: "1111"} } 
- 
+ true: "true" 
 } 
  );
           const initObj = initCt();
