9274d4a635bb06bd5b8530817e4f27578073d9b6
Carlos A. <carlosvsk@gmail.com>
2025-03-26 15:28:15 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,8 +1,21 @@
 
 // ---------- import React Packs
+import * as React from 'https://esm.sh/react@18.2.0';
+import * as ReactDOM from 'https://esm.sh/react-dom@18.2.0';
+
 // ---------- import Internals
 // import { Router } from './src';
 
 export default function App() {
   return <div>Oi Gente</div>;
 }
+
+// Render the App
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <div>Oi Pessoal</div>
+  </React.StrictMode>
+);
