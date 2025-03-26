18ebf07d1d63cf2c7d6dd840843210ed05aa71c1
Carlos A. <carlosvsk@gmail.com>
2025-03-26 14:41:56 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -4,54 +4,10 @@
           import React from 'react';
           import * as RN from 'react-native';
 
-          // ---------- import Variables Pack
-          import { create } from 'zustand';
-
-          // ---------- import Local Tools
-          import { mapElements } from './tools/base/project/mapElements';
-          import * as functions from './tools/base/functions';
-          import * as Elements from './tools/base/Elements';
-          import { Project } from './tools/base/project';
-          import * as jsvals from './tools/base/jsvals';
-          import { props } from './tools/base/props';
-          import * as customs from './tools/customs';
-          import * as stls from './tools/base/stls';
-          import { tools } from './tools';
-
-          // ---------- set Caps Inputs
-          const currRoute = 'home';
-
-          let args: any = [];
-
-          const screens = [
-            []
-          ];
-
-          const initCt = () => (
- {
- true: "true" 
-} 
- );
-          const initObj = initCt();
-          // console.log(initObj);
-
-          const arrInitFuncs = [
-            ()=>{}
-          ];
-
-          export const useRoutes = create(() => ({ currRoute }));
-          export const useData = create(() => initObj);
-
           // ---------- set Main Component
           export const Router = () => {
-            return (
-              <Project
-                configData={{
-                  screens,
-                  initCt,
-                  arrInitFuncs,
-                }}
-              />
-            );
-          };
-        
+ return (
+ <div>Oi Gente !!<div>
+);
+};
+
