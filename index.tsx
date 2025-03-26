4e5d1a068070c9c28c7156a39951a5f8316e01f1
Carlos A. <carlosvsk@gmail.com>
2025-03-26 16:23:01 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -4,18 +4,14 @@ import React from 'react';
 import * as RN from 'react-native';
 
 // ---------- import Internals
-// import { Router } from './src';
+import { Router } from './src';
 
 export default function App() {
-  return <div>Oi Gente</div>;
+  return <Router />;
 }
 
-// Render the App
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <div>Oi Pessoal</div>
-  </React.StrictMode>
-);
+RN.AppRegistry.registerComponent('App', () => App);
+
+RN.AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
