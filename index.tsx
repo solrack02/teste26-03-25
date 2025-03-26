8168a1feee68ba7860cd9f10a2a2526c4e6392bc
Carlos A. <carlosvsk@gmail.com>
2025-03-26 16:42:21 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,10 +1,16 @@
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import * as RN from 'react-native';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-)
+// ---------- import Internals
+import { Router } from './src';
+
+export default function App() {
+  return <></>;
+}
+
+RN.AppRegistry.registerComponent('App', () => App);
+
+RN.AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
